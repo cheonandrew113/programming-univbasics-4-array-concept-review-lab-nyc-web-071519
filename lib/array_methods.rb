98e1ffe7329209f0ffb9abfb687801923ce72c88
Andnrew Cheon <cheonandrew113@gmail.com>
2019-07-01 23:26:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,10 +6,10 @@ end
 
 #'takes in an array of integers and returns the highest value integer'
 def find_max_value(array)
-  find_min_value.min
+  return find_max_value.max
 end
 
 #takes in an array of integers and returns the lowest value integer
 def find_min_value(array)
-  # Add your solution here
+  return find_min_value.min
 end
